a4826e961b500b21f7cfa6c86acb8d661b88082d
modyskyline <modyskyline@live.com>
2017-12-21 20:04:26 +0200

    Tut12_ math_overload update

--- Tut12_ math_overload/main.cpp
+++ Tut12_ math_overload/main.cpp
@@ -10,25 +10,30 @@ using namespace std;
 
 int main()
 {
+	/*Create objects*/
 	Time_Base t(0,0,0);
 	Time_Base t1(1,1,1);
 
+	/*Testing Adding exp*/
 	cout << "\n\nTesting Adding exp:\n";
 	t=t+t1;
 	cout << " t is "<< t<<endl;
 
 
+	/*Testing the prefix increment operator*/
 	cout << "\n\nTesting the prefix increment operator:\n"
 		 << " t is "<< t << endl;
 	cout << "++t is "<< ++t << endl;
 	cout << " t is "<< t<<endl;
 
+	/*Testing the postfix increment operator*/
 	cout << "\n\nTesting the postfix increment operator:\n"
 		 << " t is "<< t << endl;
 	cout << "t++ is "<< t++ << endl;
 	cout << " t is "<< t<<endl;
 
 
+	/*Testing Adding value*/
 	cout << "\n\nTesting Adding value:\n"
 		 << " t is "<< t << endl;
 	cout << "t+3 is "<< t+3 << endl;
