68a7e3b4c971cf617fd47dda94ce085df17a156a
modyskyline <modyskyline@live.com>
2017-12-21 17:06:47 +0200

    Tut3_ Calling constructor_destructor_return Ref update

--- Tut3_ Calling constructor_destructor_return Ref/main.cpp
+++ Tut3_ Calling constructor_destructor_return Ref/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 void create (void);
 
+/*Creation of object 1(global)*/
 Time_Base t1(1,1,1,1);
 
 int main()
@@ -21,25 +22,35 @@ int main()
 
 	cout << "\nMAIN FUNCTION: EXECUTION BEGINS"<< endl;
 
+	
 	cout << "\n Object size depends on data members which are 4 int"<< endl;
+
+	/*Show the size of one element member data*/
 	cout<<"The size of one int is -->"<<sizeof(int)<<endl;
+
+	/*Show the size of the object*/
 	cout<<"The size of object is -->"<<sizeof(t1)<<endl;
 
+	/*Creation of object 2(local)*/
 	Time_Base t2 (2,2,2,2);
+
+	/*Creation of object 3(static local)*/
 	static Time_Base t3(3,3,3,3);
 
 	create();
 
 	cout << "\nMAIN FUNCTION: Main resume"<< endl;
+
+	/*Creation of object 4(local)*/
 	Time_Base t4 (4,4,4,4);
-
-
 }
 
 void create (void)
 {
 	cout << "\ncreate FUNCTION: EXECUTION BEGINS"<< endl;
+	/*Creation of object 5(local)*/
 	Time_Base t5 (5,5,5,5);
+	/*Creation of object 6(static local)*/
 	static Time_Base t6(6,6,6,6);
 	cout << "\ncreateFUNCTION: EXECUTION ENDS"<< endl;
 }
