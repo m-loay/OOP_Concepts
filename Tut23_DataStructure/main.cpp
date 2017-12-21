fd9f22365e2d9d0c664899a351091773e4143492
modyskyline <modyskyline@live.com>
2017-12-21 21:59:03 +0200

    Tut23_DataStructure

--- Tut23_DataStructure/main.cpp
+++ Tut23_DataStructure/main.cpp
@@ -14,7 +14,44 @@ using namespace std;
 int main ()
 {
 
+
+	Slist<int> numS;
+	Stack<int> numST;
 	Qeue<int> numQ;
+
+	/*Single linked list operation*/
+	cout<<"Single linked list operation"<<endl;
+	numS.insertFront(1);
+	numS.insertBack(2);
+	numS.insertBack(3);
+	numS.insertFront(4);
+	numS.insertFront(5);
+	numS.print();
+
+	numS.removeBack();
+	numS.removeFront();
+	numS.print();
+
+	numS.removeBack();
+	numS.print();
+
+	/*Stack operation*/
+	cout<<"Stack operation"<<endl;
+	numST.push(1);
+	numST.push(2);
+	numST.push(3);
+	numST.push(4);
+	numST.push(5);
+	numST.printStack();
+
+	numST.pop();
+	numST.printStack();
+
+	numST.pop();
+	numST.printStack();
+
+	/*Qeue operation*/
+	cout<<"Qeue operation"<<endl;
 	numQ.enqeue(1);
 	numQ.enqeue(2);
 	numQ.enqeue(3);
