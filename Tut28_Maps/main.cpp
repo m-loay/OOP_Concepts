7ea616a4b1b74d6e815bd7bf0a4d303026747b67
modyskyline <modyskyline@live.com>
2017-12-23 17:46:57 +0200

    Tut28_Maps

--- Tut28_Maps/main.cpp
+++ Tut28_Maps/main.cpp
@@ -5,27 +5,47 @@
  *      Author: modys
  */
 
-
 #include <iostream>
 #include <map>
 #include <iterator>
 using namespace std;
+typedef map<int, double> MapInt;
 
-typedef map<string, string> MapString;
+void printMap (const map<int, double> &Map );
+
 int main ()
 {
-	MapString family;
-	family.insert(MapString::value_type("Father","x"));
-	family.insert(MapString::value_type("Mother","y"));
-	family.insert(MapString::value_type("Son","Mody"));
-	family.insert(MapString::value_type("F.Daughter","z"));
-	family.insert(MapString::value_type("S.Daughter","s"));
-	family.insert(MapString::value_type("S.Daughter","s"));
-	family.insert(MapString::value_type("S.Daughter","s"));
+	MapInt pairs;
 
-	for(MapString::const_iterator it = family.begin(); it != family.end(); ++it)
-	{
-		cout<<it->first<<" is "<<it->second<<endl;
-	}
+	/*Insert eight value type object in pairs*/
+	pairs.insert(MapInt::value_type(1 ,1.1));
+	pairs.insert(MapInt::value_type(2 ,2.2));
+	pairs.insert(MapInt::value_type(3 ,3.3));
+	pairs.insert(MapInt::value_type(4 ,4.4));
+	pairs.insert(MapInt::value_type(5 ,5.5));
+	pairs.insert(MapInt::value_type(3 ,6.6)); //dup value
+	pairs.insert(MapInt::value_type(7 ,7.7)); 
+	pairs.insert(MapInt::value_type(1 ,8.8)); //dup value
+
+	/*print the map content*/
+	cout<<"pairs contains: \nKeys\tValue"<<endl;
+	printMap(pairs);
+
+	/*Change the map content*/
+	pairs[1] = 10.10;
+	pairs[5] = 50.50;
+
+	/*print the map content*/
+	cout<<"pairs contains: \nKeys\tValue"<<endl;
+	printMap(pairs);
 }
 
+void printMap (const map<int, double> &Map)
+{
+	typename map<int, double>::const_iterator const_it;
+
+	for(const_it = Map.begin() ; const_it != Map.end() ; const_it++)
+	{
+		cout<<const_it->first << '\t' << const_it->second <<endl;
+	}
+}
