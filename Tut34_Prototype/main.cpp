8f71776543b0672e281d14be7604ead4c9485bc9
modyskyline <modyskyline@live.com>
2017-12-24 17:41:37 +0200

    Tut34_Prototype

--- Tut34_Prototype/main.cpp
+++ Tut34_Prototype/main.cpp
@@ -7,25 +7,31 @@
 
 #include <iostream>
 #include <vector>
-#include "Widget.h"
+#include "protoType.h"
 using namespace std;
 
 int main()
 {
-	vector<Widget*>roles;
+	/*Create vector of pointers to class shape*/
+	vector<Shape*>roles;
+
+	/*Selection paramters*/
 	int choice;
+
+	/*Create a loop to save a base pointers(shape) to desired derived objects*/
 	while(true)
 	{
-		cout << "LinuxButton(0) LinuxMenu(1) WindowsButton(2) WindowsMenu(3) Go(4): "<<endl;
+		cout << "Circle(0) Square(1) Rectangle(2) Go(3): "<<endl;
 		cin >> choice;
-		if (choice == 4)
+		if (choice == 3)
 			break;
-		roles.push_back(Factory::make_Widget(choice));
+		roles.push_back(Prototype::getPrototype(choice));
 	}
 
 	for (unsigned int i = 0; i < roles.size(); i++)
 		roles[i]->draw();
 
+	/*Free all allocation*/
 	for (unsigned int i = 0; i < roles.size(); i++)
 		delete roles[i];
 }
---
+++ Tut34_Prototype/protoType.h
@@ -0,0 +1,121 @@
+/*
+ * OsFactory.h
+ *
+ *  Created on: Oct 23, 2016
+ *      Author: modys
+ */
+/*
+ * protoType.h
+ *
+ *  Created on: Oct 23, 2016
+ *      Author: modys
+ */
+
+#ifndef PROTOTYPE_H_
+#define PROTOTYPE_H_
+
+using namespace std;
+
+class Shape 
+{
+	public:
+		Shape()
+		{
+			cout<<"Shape constructor called"<<endl;
+		}
+
+		virtual ~Shape()
+		{
+			cout<<"Shape destructor called"<<endl;
+		}
+
+		virtual void draw()=0;
+		virtual Shape *clone()=0;
+};
+
+
+class Circle : public Shape 
+{
+	public:
+		Circle()
+		{
+			cout<<"Circle constructor called"<<endl;	
+		}
+
+		~Circle()
+		{
+			cout<<"Circle destructor called"<<endl;
+		}
+
+		void draw() { cout << "Circle\n"; }
+
+		Circle *clone(){return new Circle;}
+};
+
+class Square : public Shape 
+{
+	public:
+		Square()
+		{
+			cout<<"Square constructor called"<<endl;	
+		}
+
+		~Square()
+		{
+			cout<<"Square destructor called"<<endl;
+		}
+
+		void draw() { cout << "Square\n"; }
+
+		Square *clone(){return new Square ; }
+};
+
+
+class Rectangle : public Shape 
+{
+	public:
+		Rectangle()
+		{
+			cout<<"Rectangle constructor called"<<endl;	
+		}
+
+		~Rectangle()
+		{
+			cout<<"Rectangle destructor called"<<endl;
+		}
+
+		void draw() { cout << "Rectangle\n"; }
+
+		Rectangle *clone(){return new Rectangle ; }
+};
+
+class Prototype
+{
+	public:
+		Prototype()
+		{
+			cout<<"Prototype constructor called"<<endl;
+		}
+		virtual ~Prototype()
+		{
+			cout<<"Prototype destructor called"<<endl;
+		}
+		static Shape *getPrototype(int choice);
+
+	private:
+		static Shape* s_prototypes[4];
+};
+
+Shape *Prototype::getPrototype(int choice)
+{
+	return s_prototypes[choice]->clone();
+}
+
+Shape *Prototype::s_prototypes[]=
+{
+		new Circle ,new Square , new Rectangle 
+};
+
+
+
+#endif /* PROTOTYPE_H_ */
