02c008bc3eeeeec3f3f799037240cdfb05860ce7
modyskyline <modyskyline@live.com>
2017-12-24 20:48:44 +0200

    DECORATOR

--- Tut38_Decorator/main.cpp
+++ Tut38_Decorator/main.cpp
@@ -8,16 +8,19 @@
 
 int main ()
 {
+	/*Creating two objects normal object and decorated object*/
 	Shape *circle = new Circle;
 
 	RedShapeDecorator *redCircle = new RedShapeDecorator(new Circle);
 
+	/*See the functionality of both normal object or decorated object*/
 	cout<<"Basic Functionality"<<endl;
 	circle->draw();
 
 	cout<<"Decorator Functionality"<<endl;
 	redCircle->draw();
 
+	/*Free Allocation*/
 	delete redCircle;
 	delete circle;
 }
