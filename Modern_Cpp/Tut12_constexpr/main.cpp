dc1526906959c55e3e42426c7d2ed06e82bb79e6
modyskyline <modyskyline@live.com>
2020-07-23 11:16:49 +0200

    Add constexpr tutorial.

---
+++ Modern_Cpp/Tut12_constexpr/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+constexpr int GetNumber() {
+	return 42;
+}
+constexpr int Add(int x, int y) {
+	return x + y;
+}
+constexpr int Max(int x, int y) {
+	//Needs C++14 compiler
+	//if (x > y)
+	//	return x;
+	//return y;
+	return x > y ? x : y;
+}
+
+int main() 
+{
+	//Behaves as a constexpr function
+	constexpr int i = GetNumber();
+	int arr[i];
+
+	//Behaves as a constexpr function
+	const int j = GetNumber();
+	int arr1[j];
+
+	//Behaves as a normal function
+	int x = GetNumber();
+
+	//Error x is not constexpr
+	//constexpr int sum = Add(x, 5);
+
+	//Behaves as a normal function. 
+	int sum2 = Add(3, 5);
+
+	return 0;
+}
