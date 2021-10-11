001c4ff2885b041aaaa83f8e139353c1eae64d86
mody <mohamed.loay.ali@gmail.com>
2021-10-11 19:14:30 +0200

    Tut3:update

--- Modern_Cpp/Tut3_Delegated_Constructor/main.cpp
+++ Modern_Cpp/Tut3_Delegated_Constructor/main.cpp
@@ -8,22 +8,28 @@
 class Car
 {
 	public:
+	/*init() is not a constructor, 
+	  it can be called during the normal program flow, 
+	  where member variables may already be set and 
+	  dynamically allocated memory may already be allocated. */
+	void init()
+	{
+		std::cout<<"Init function"<<std::endl;
+		fuel_ = 40;
+		passenger_ = 3;
+	}
 	Car()
 	{
+		init();
+		speed_ = 0;
 		std::cout<<"Car()"<<std::endl;
 	}
 
-	Car(int amount)
+	Car(int speed)
 	{
-		fuel_ = amount;
-		std::cout<<"Car(int amount)"<<std::endl;
-	}
-
-	Car(int amount , int pass)
-	{
-		fuel_ = amount;
-		passenger_ = pass;
-		std::cout<<"Car(int amount , int pass)"<<std::endl;
+		init();
+		speed_ = speed;
+		std::cout<<"Car(int speed)"<<std::endl;
 	}
 
 	void dasbord()const
@@ -31,7 +37,7 @@ class Car
 		std::cout<<"car Dash Board"<<std::endl;
 		std::cout<<"Fuel in car = "<<fuel_<<std::endl;
 		std::cout<<"Passengers in car = "<<passenger_<<std::endl;
-		std::cout<<"speed of car = "<<passenger_<<std::endl;
+		std::cout<<"speed of car = "<<speed_<<std::endl;
 	}
 
 	private:
@@ -43,22 +49,15 @@ class Car
 class Vehicle
 {
 	public:
-	Vehicle():Vehicle(0)
+	Vehicle()
 	{
 		std::cout<<"Vehicle()"<<std::endl;
 	}
 
-	Vehicle(int amount):Vehicle(amount,0)
+	Vehicle(int speed):Vehicle()
 	{
-		fuel_ = amount;
-		std::cout<<"Vehicle(int amount)"<<std::endl;
-	}
-	
-	Vehicle(int amount , int pass)
-	{
-		fuel_ = amount;
-		passenger_ = pass;
-		std::cout<<"Vehicle(int amount , int pass)"<<std::endl;
+		speed_ = speed;
+		std::cout<<"Vehicle(int speed)"<<std::endl;
 	}
 
 	void dasbord()const
@@ -66,17 +65,18 @@ class Vehicle
 		std::cout<<"Vehicle Dash Board"<<std::endl;
 		std::cout<<"Fuel in Vehicle = "<<fuel_<<std::endl;
 		std::cout<<"Passengers in Vehicle = "<<passenger_<<std::endl;
-		std::cout<<"speed of Vehicle = "<<passenger_<<std::endl;
+		std::cout<<"speed of Vehicle = "<<speed_<<std::endl;
 	}
 
 	private:
-	int fuel_{0};
-	int passenger_{0};
+	int fuel_{40};
+	int passenger_{3};
 	float speed_{0.0f};
 };
 
 int main()
 {
+	std::cout<<"\n -----Use default constructor-----"<<std::endl;
 	//create object from both classes
 	Car c;
 	Vehicle v;
@@ -84,4 +84,12 @@ int main()
 	//view dash board
 	c.dasbord();
 	v.dasbord();
+
+	std::cout<<"\n -----Use paramertrized constructor-----"<<std::endl;
+	Car c2(5);
+	Vehicle v2(5);
+
+	//view dash board
+	c2.dasbord();
+	v2.dasbord();
 }
