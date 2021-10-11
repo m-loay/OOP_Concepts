eb99958f1e3c54b57f1e19d4463d18ecbd100d28
mody <mohamed.loay.ali@gmail.com>
2021-10-11 04:28:48 +0200

    Tut17:update

---
+++ Basics/Tut16_Derived_ClassMemberFunction_CallsViaBaseClassPointersClass/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 23, 2016
+ *      Author: modys
+ *      The purpose of the tutorial
+ *
+ */
+
+#include <iostream>
+#include "CommissionEmployee.h"
+#include "BasePlus.h"
+using namespace std;
+
+int main ()
+{
+    //create base class pointer
+    CommissionEmployee *comEmp_ptr=0;
+
+    //create object from derived class
+    BasePlus baseEmp("Samer","Saber","22-22",5000,0.04,300);
+    BasePlus *baseEmp_ptr=0;
+
+    //Assign a base class pointer to derived object
+    comEmp_ptr=&baseEmp;
+
+    cout<<comEmp_ptr->getFirstName()<<' '<<comEmp_ptr->getLastName()<<endl;
+
+    //Calling earning using base pointer object
+    //earning = getComissionRate() * getGrossSales();
+    cout<<"\n ------Calling earning using base pointer object------\n"<<endl;
+    cout<<comEmp_ptr->earnings()<<endl;
+
+    //Calling earning using derived pointer object
+    //earning= getBaseSalary() + CommissionEmployee::earnings();
+    cout<<"\n ------Calling earning using derived pointer object------\n"<<endl;
+    baseEmp_ptr=&baseEmp;
+    cout<<baseEmp_ptr->earnings()<<endl;
+
+
+    //This will generate an error to call member calss not inherited from base class
+    //comEmp_ptr->setsetBaseSalaray(400);
+}
--- Basics/Tut17_VirtualFunction/main.cpp
+++ Basics/Tut17_VirtualFunction/main.cpp
@@ -25,7 +25,7 @@ int main ()
 	BasePlus *ptr_bemp=&bemp;
 
 	/*Use static binding to print data for both objects*/
-	cout<<"Static binding"<<endl;
+	cout<<"\n ----Static binding----\n"<<endl;
 	cemp.print();
 	bemp.print();
 
@@ -33,7 +33,7 @@ int main ()
 	 * base pointer to base object
 	 * derived pointer to derived object*/
 
-	cout<<"Pointer printing"<<endl;
+	cout<<"\n ----Pointer printing---- \n"<<endl;
 	ptr_cemp ->print();
 	ptr_bemp ->print();
 
@@ -41,7 +41,7 @@ int main ()
    * when a base pointer to derived object is used ,the compiler automatically
    * selects the member function of object type (not the pointer type object) and call it.
    * */
-  cout<<"Using a base pointer to derived object"<<endl;
+  cout<<"\n ----Using a base pointer to derived object----\n"<<endl;
   ptr_cemp = &bemp;
   ptr_cemp ->print();
 }
