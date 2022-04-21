f53bb1d4cec58f011859d3d2b9bbabb089dd84ef
Mohamed Loay Ali <mlali@beamab.com>
2022-04-21 15:01:12 +0200

    update Basics/Tut3

--- Basics/Tut3_DefaultArguments_FunctionTemp/Boxvol.cpp
+++ Basics/Tut3_DefaultArguments_FunctionTemp/Boxvol.cpp
@@ -9,48 +9,48 @@
 
 Box_vol::Box_vol()
 {
-	// TODO Auto-generated constructor stub
-	setDim(1,1,1);
+    // TODO Auto-generated constructor stub
+    setDim(1,1,1);
 }
 
 Box_vol::~Box_vol() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
 
 void Box_vol:: setDim(int length, int width, int height)
 {
-	len=length;
-	wid=width;
-	high=height;
+    len=length;
+    wid=width;
+    high=height;
 }
 
 int Box_vol:: getVolume(void)
 {
-	return len * wid * high;
+    return len * wid * high;
 }
 
 void Box_vol:: print_volume (void)
 {
-	cout<<"volume= "<<getVolume()<<endl;
+    cout<<"volume= "<<getVolume()<<endl;
 }
 
 void Box_vol:: max_valueInt(int op1, int op2, int op3)
 {
-	biggest_int=maximum(op1, op2, op3);
+    biggest_int=maximum(op1, op2, op3);
 }
 
 int Box_vol:: getBiggestInt(void)
 {
-	return biggest_int;
+    return biggest_int;
 }
 
 void Box_vol:: max_valueDouble(double op1, double op2, double op3)
 {
-	biggest_double=maximum(op1, op2, op3);
+    biggest_double=maximum(op1, op2, op3);
 }
 
 double Box_vol:: getBiggestDouble(void)
 {
-	return biggest_double;
+    return biggest_double;
 }
 
--- Basics/Tut3_DefaultArguments_FunctionTemp/main.cpp
+++ Basics/Tut3_DefaultArguments_FunctionTemp/main.cpp
@@ -13,45 +13,44 @@ int var(3);
 
 int main ()
 {
+    Box_vol volume_box;
+    volume_box.print_volume();
 
-	Box_vol volume_box;
-	volume_box.print_volume();
+    /*Default Arguments*/
 
-	/*Default Arguments*/
+    /*Default Arguments, set without using any input arguments*/
+    volume_box.setDim();
+    volume_box.print_volume();
 
-	/*Default Arguments, set without using any input arguments*/
-	volume_box.setDim();
-	volume_box.print_volume();
+    /*Default Arguments, set the first arguments only*/
+    volume_box.setDim(10);
+    volume_box.print_volume();
 
-	/*Default Arguments, set the first arguments only*/
-	volume_box.setDim(10);
-	volume_box.print_volume();
+    /*Default Arguments , set the first and second arguments*/
+    volume_box.setDim(10,5);
+    volume_box.print_volume();
 
-	/*Default Arguments , set the first and second arguments*/
-	volume_box.setDim(10,5);
-	volume_box.print_volume();
+    /*Default Arguments ,  set all arguments*/
+    volume_box.setDim(10,5,2);
+    volume_box.print_volume();
 
-	/*Default Arguments ,  set all arguments*/
-	volume_box.setDim(10,5,2);
-	volume_box.print_volume();
+    /*Function Template*/
 
-	/*Function Template*/
+    /*given to function integers arguments*/
+    volume_box.max_valueInt(10,5,2);
+    cout << "dBiggest int: "<<volume_box.getBiggestInt() <<endl;// prompt user for data
 
-	/*given to function integers arguments*/
-	volume_box.max_valueInt(10,5,2);
-	cout << "dBiggest int: "<<volume_box.getBiggestInt() <<endl;// prompt user for data
+    /*given to function floats arguments*/
+    volume_box.max_valueDouble(10.5,5.3,2.1);
+    cout << "dBiggest double: "<<volume_box.getBiggestDouble() <<endl;// prompt user for data
 
-	/*given to function floats arguments*/
-	volume_box.max_valueDouble(10.5,5.3,2.1);
-	cout << "dBiggest double: "<<volume_box.getBiggestDouble() <<endl;// prompt user for data
+    /*compare between local and global scope*/
+    {
+        int var(6);
+        std::cout<<"Printing var in local scope, var = "<<var<<std::endl;
 
-	/*compare between local and global scope*/
-	{
-		int var(6);
-		std::cout<<"Printing var in local scope, var = "<<var<<std::endl;
-
-		std::cout<<"Printing var in global scope ::var= "<<::var<<std::endl;
-	}
+        std::cout<<"Printing var in global scope ::var= "<<::var<<std::endl;
+    }
 
 }
 
