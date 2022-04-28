a9706c87f39bbd54789c59d618fed3df36c494b5
MohamedLoayAli <MohamedLoayAli@BEAM-N-006>
2022-04-28 17:10:45 +0200

    update build system for repo.

--- Basics/Tut1_pointers_and_refParamters/TimeBase.cpp
+++ Basics/Tut1_pointers_and_refParamters/TimeBase.cpp
@@ -10,49 +10,53 @@
 #include <iostream>
 
 
-Time_Base::Time_Base(const int &ref = 0):counter(const_cast<int &>(ref))
+Time_Base::Time_Base(const int &ref):counter_(const_cast<int &>(ref))
 {
-    std::cout<<"Time Base Constructor"<<std::endl;
-    // TODO Auto-generated constructor stub
+	std::cout<<"Time Base Constructor"<<std::endl;
 }
 
 Time_Base::~Time_Base()
 {
-    std::cout<<"Time Base Destructor"<<std::endl;
-    // TODO Auto-generated destructor stub
+	std::cout<<"Time Base Destructor"<<std::endl;
+
 }
 
-void Time_Base:: setTime (int h, int m, int s)
+void Time_Base::setTime(int h, int m, int s)
 {
-    hour=(h>=0 && h<24)? h:0;
-    minute= (m>=0 && m<60)? m:0;
-    second= (s>=0 && s<60) ? s:0;
+	hr_  = (h>= 0 && h <24)?h:0;
+	min_ = (m>= 0 && m <60)?m:0;
+	sec_ = (s>= 0 && s <24)?s:0;
 }
 
-void Time_Base:: setCounter (const int &ref)
+void Time_Base::setCounter(const int &ref)
 {
-    counter = const_cast<int &>(ref);
+	counter_ = const_cast<int&>(ref);
 }
 
-int &Time_Base::getCounter()
+int& Time_Base::getCounter()
 {
-    return counter;
+	return counter_;
 }
 
- int  &Time_Base:: badSetHour(int hh)
+int& Time_Base::badSetHour(int hh)
 {
-    hour = ( hh >= 0&& hh < 24) ? hh : 0;
-    return hour; // DANGEROUS reference return
+	hr_  = (hh>= 0 && hh <24)?hh:0;
+	return hr_; // DANGEROUS reference return
 }
 
-void Time_Base:: printUniversal()
+void Time_Base::printUniversal()
 {
-    std::cout<<setfill('0')<<setw(2)<<hour<<":"<<
-            setw(2)<<minute<<":"<<
-            setw(2)<<second<<std::endl;
+	std::cout<<setfill('0')<<setw(2)<<hr_<<":"<<
+			setw(2)<<min_<<":"<<
+			setw(2)<<sec_<<std::endl;
+
 }
-void Time_Base:: printStandard ()
+
+
+void Time_Base::printStandard()
 {
-    std::cout << ((hour == 0|| hour== 12) ? 12: hour % 12) <<":"<<setfill('0')<<setw(2)<<minute<<":"<<
-            setw(2)<<second<<(hour < 12? " AM": " PM")<<std::endl;
+	std::cout << ((hr_ == 0|| hr_== 12) ? 12: hr_ % 12) <<":"<<setfill('0')
+							<<setw(2)<<min_<<":"<<setw(2)<<sec_
+							<<(hr_ < 12? " AM": " PM")<<std::endl;
+
 }
--- Basics/Tut1_pointers_and_refParamters/TimeBase.h
+++ Basics/Tut1_pointers_and_refParamters/TimeBase.h
@@ -14,21 +14,21 @@ using namespace std;
 class Time_Base
 {
 public:
-    Time_Base(const int &ref);
-    ~Time_Base();
-    void setTime (int h, int m, int s);
-    void setCounter (const int &ref);
-    int &getCounter ();
+	Time_Base(const int &ref = 0);
+	~Time_Base();
+	void setTime (int h, int m, int s);
+	void setCounter (const int &ref);
+	int &getCounter ();
 
-    int &badSetHour(int hh);
-    void printUniversal();
-    void printStandard ();
+	int &badSetHour(int hh);
+	void printUniversal();
+	void printStandard ();
 
 private:
-    int hour{0};
-    int minute{0};
-    int second{0};
-    int & counter;
+	int hr_{0};
+	int min_{0};
+	int sec_{0};
+	int& counter_;
 };
 
 #endif /* TIMEBASE_H_ */
